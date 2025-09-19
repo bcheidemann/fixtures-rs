fba614976aa4ca3af2219521c69891f3c5c70cb1
Ben Heidemann <ben@heidemann.dev>
2025-09-19 12:25:58 +0100

    fix: deprecate watch_fixture_dir(s) in favour of watch_dir(s)
    This is more concise and still explicit with the module prefix
    `fixtures::build::watch_dir(s)`.

--- fixtures/src/build.rs
+++ fixtures/src/build.rs
@@ -1,14 +1,24 @@
 use std::path::Path;
 
-pub fn watch_fixture_dir<P: AsRef<Path>>(path: P) {
+pub fn watch_dir<P: AsRef<Path>>(path: P) {
     println!(
         "cargo:rerun-if-changed={}",
         path.as_ref().to_str().expect("path should be valid UTF-8")
     );
 }
 
-pub fn watch_fixture_dirs<P: AsRef<Path>>(paths: &[P]) {
+pub fn watch_dirs<P: AsRef<Path>>(paths: &[P]) {
     for path in paths {
-        watch_fixture_dir(path);
+        watch_dir(path);
     }
+}
+
+#[deprecated(since = "2.1.2", note = "Use `fixtures::build::watch_dir` instead.")]
+pub fn watch_fixture_dir<P: AsRef<Path>>(path: P) {
+    watch_dir(path);
+}
+
+#[deprecated(since = "2.1.2", note = "Use `fixtures::build::watch_dirs` instead.")]
+pub fn watch_fixture_dirs<P: AsRef<Path>>(paths: &[P]) {
+    watch_dirs(paths);
 }
